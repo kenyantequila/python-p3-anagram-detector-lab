5f94717257ea86a2d7fecda5f344bd8843e985c6
kenyantequila <abdishakur431@gmail.com>
2024-05-29 06:58:27 +0300

    commit

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,17 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, words):
+        anagrams = []
+        #Use the sorted functionality to arrange the letters alphabetically
+        sorted_word = sorted(self.word)
+        # Using for loop to iterte over a=each string.
+        for w in words:
+            # If a words has the same letters as self.word then it is an anagram 
+            if sorted(w) == sorted_word:
+                anagrams.append(w)
+        return anagrams
+
+        
